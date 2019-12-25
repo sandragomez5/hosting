d274a4012cab66f6fb218a612230093c8f2d106b
sandragomez5 <58513896+sandragomez5@users.noreply.github.com>
2019-12-25 12:50:33 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,38 +4,13 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/wooden-spoon-4546584_960_720.png.png') {
-    myImage.setAttribute ('src','images/tortilla-622974_960_720.png');
+  if(mySrc === 'images/cookies.png') {
+    myImage.setAttribute ('src','images/ingredients.png');
   } else {
-    myImage.setAttribute ('src','images/wooden-spoon-4546584_960_720.png.png');
+    myImage.setAttribute ('src','images/cookies.png');
   }
 }
 
-// Personalized welcome message code
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Inserta tu apodo. El que más te guste. ;-)');
-  if(!myName || myName === null) {
-    
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Bienvenido a MiVä´s World, ' + myName + '. ¡Disfruta!';
-  }
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Bienvenido a MiVä´s World, ' + storedName + '. ¡Disfruta!';
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
 
 // El gato desplazándose <3
 var catEl = document.getElementById("cat");
