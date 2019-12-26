7d963de8993ab725322010275b38d61b7c8fa69e
sandragomez5 <58513896+sandragomez5@users.noreply.github.com>
2019-12-26 14:37:21 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/cookies.png') {
-    myImage.setAttribute ('src','images/ingredients.png');
-  } else {
+  if(mySrc === 'images/ingredients.png') {
     myImage.setAttribute ('src','images/cookies.png');
+  } else {
+    myImage.setAttribute ('src','images/ingredients.png');
   }
 }
 
